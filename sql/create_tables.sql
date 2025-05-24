32066bc08904125dce546280ae87207a50508e76
Chen-0317 <139035663+Chen-0317@users.noreply.github.com>
2025-05-24 16:19:13 +0800

    Add files via upload

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,18 +1,21 @@
+-- /sql/create_tables.sql
 CREATE TABLE IF NOT EXISTS symbols (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    symbol TEXT UNIQUE,
-    name TEXT,
-    market TEXT
+    symbol TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    region TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS price_data (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    symbol TEXT,
-    date TEXT,
+    symbol_id INTEGER NOT NULL,
+    date TEXT NOT NULL,
     open REAL,
     high REAL,
     low REAL,
     close REAL,
     volume INTEGER,
-    UNIQUE(symbol, date)
+    UNIQUE(symbol_id, date),
+    FOREIGN KEY(symbol_id) REFERENCES symbols(id)
 );
